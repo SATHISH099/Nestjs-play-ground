71546d488f26e55a0bef4a7e733c4c0d519f25ca
SATHISH099 <sathish2lmes@gmail.com>
2024-02-17 16:20:27 +0530

    feat: blocking and non-blocking req api

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { resolve } from 'path';
 
 @Controller('api')
 export class AppController {
@@ -8,5 +9,21 @@ export class AppController {
   @Get('home')
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('blocking')
+  getBlocking(): string {
+    const time = new Date().getTime();
+    while (new Date().getTime() < time + 10000) {}
+    return 'blocking';
+  }
+
+  @Get('non-blocking')
+  async getNonBlocking(): Promise<string> {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve('non-blocking');
+      }, 10000);
+    });
   }
 }
