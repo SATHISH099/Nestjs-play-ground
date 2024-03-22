7836de4fbb0a06de34a04a60bd85fb5b60da2d34
SATHISH099 <sathish2lmes@gmail.com>
2024-03-22 18:45:45 +0530

    chore: connect db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DATABASE_HOST,
+      port: Number(process.env.DATABASE_PORT),
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      entities: [],
+      synchronize: false,
+      autoLoadEntities: true,
+      retryAttempts: 2,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
